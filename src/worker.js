9218f5ce28ac420843f71d8b29f85b574605fb30
Jadranko Dragoje <dragoje.jadranko@nsoft.com>
2018-04-25 10:24:37 +0200

    Add push method

--- src/worker.js
+++ src/worker.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
-import { defaults, template } from 'lodash';
+import cryptoJS from 'crypto-js';
+import { assign, defaults, template } from 'lodash';
 
 /*!
 * Pusher Web Worker
@@ -7,10 +8,31 @@ import { defaults, template } from 'lodash';
 */
 
 const apiEndpoint = 'http://api-${cluster}.pusher.com';
-const apiAuthVersion = '1.0';
 
 const defaultOptions = {
   cluster: 'eu',
+  version: '1.0',
+};
+
+const getAuthTimestamp = () => {
+  const currentDate = new Date();
+  return Math.round(currentDate.getTime() / 1000);
+};
+
+const getAuthSignature = (method, params, options) => {
+  const authSignatureRaw = `${method}\n
+    /apps/${options.appId}/events\n
+    auth_key=${params.auth_key}
+    &auth_timestamp=${params.auth_timestamp}
+    &auth_version=${params.auth_version}
+    &body_md5=${params.body_md5}`;
+
+  return cryptoJS.HmacSHA256(authSignatureRaw, options.secret).toString(cryptoJS.enc.Hex);
+};
+
+const getBodyMD5 = (payload) => {
+  const encryptedPayload = cryptoJS.MD5(payload);
+  return encryptedPayload.toString();
 };
 
 const sendEvent = (payload, options) => {
@@ -20,20 +42,37 @@ const sendEvent = (payload, options) => {
     cluster: options.cluster,
   });
 
+  const authTimestamp = getAuthTimestamp();
+
+  const params = {
+    auth_timestamp: authTimestamp,
+    auth_version: options.version,
+    body_md5: getBodyMD5(payload),
+    auth_key: options.key,
+  };
+
+  const payloadData = payload.data || null;
+  assign(payload, {
+    data: JSON.stringify(payloadData),
+  });
+
+  params.auth_signature = getAuthSignature('POST', params, options);
+
   axios.post(`${endpoint}/apps/${options.appId}/events`, payload, {
-    params: {
-      auth_timestamp: null,
-      auth_version: apiAuthVersion,
-      body_md5: null,
-      auth_key: options.authKey,
-      auth_signature: null,
+    params,
+    headers: {
+      'Content-Type': 'application/json',
     },
+  }).catch((error) => {
+    const errorMessage = error.response ? error.response.data : error;
+    /* eslint no-console: "off" */
+    console.warn('Pusher worker sendEvent error', errorMessage);
   });
 };
 
 const logEvent = (message) => {
   /* eslint no-console: "off" */
-  console.warn('Unknown action', message.action);
+  console.warn('Pusher worker unknown action', message.action);
 };
 
 self.onmessage = (event) => {
@@ -47,4 +86,9 @@ self.onmessage = (event) => {
         logEvent(message);
     }
   }
+};
+
+self.onerror = (event) => {
+  /* eslint no-console: "off" */
+  console.warn('Pusher worker error', event);
 };
